e81486d845dbae1f27f4002804e95026dca4a0ba
Perisync <dev@Perisyncs-MacBook-Pro.local>
2024-01-09 10:29:26 +0530

    c

--- js/screens/auth/onboarding/component/TvScreen.js
+++ js/screens/auth/onboarding/component/TvScreen.js
@@ -2,7 +2,7 @@ import React from 'react';
 import { View, StyleSheet, Dimensions } from 'react-native';
 import SafeAreaView from 'react-native-safe-area-view';
 
-import { margin , flex, heightValue, position, radius, styles, widthValue} from '../../../../styles/Styles';
+import { margin } from '../../../../styles/Styles';
 margin
 const { width, height } = Dimensions.get('window');
 
@@ -24,8 +24,8 @@ const styless = StyleSheet.create({
     // Add any additional styles for the container if needed
   },
   tvscreenMain: {
-    width: width * 1, // 80% of the screen width
-    height: height * 0.5, // 60% of the screen height
+    width: width * 1, 
+    height: height * 0.5, 
     backgroundColor: "#ff6347",
     borderTopLeftRadius: 15,
     borderTopRightRadius: 15,
@@ -33,43 +33,43 @@ const styless = StyleSheet.create({
     borderBottomLeftRadius: 15,
   },
   tvscreenTop: {
-    width: width * 0.6, // 40% of the screen width
-    height: height * 0.35, // 35% of the screen height
+    width: width * 0.6,
+    height: height * 0.35, 
     backgroundColor: "#ff6347",
     position: "absolute",
-    top: -height * 0.15, // 15% of the screen height
-    left: width * 0.2, // 20% of the screen width
-    borderRadius: height * 0.175, // 17.5% of the screen height
+    top: -height * 0.15, 
+    left: width * 0.2,
+    borderRadius: height * 0.175, 
     transform: [{ scaleX: 2 }, { scaleY: 0.5 }],
   },
   tvscreenBottom: {
-    width: width * 0.6, // 40% of the screen width
-    height: height * 0.35, // 35% of the screen height
+    width: width * 0.6, 
+    height: height * 0.35, 
     backgroundColor: "#ff6347",
     position: "absolute",
-    bottom: -height * 0.15, // 15% of the screen height
-    left: width * 0.2, // 20% of the screen width
-    borderRadius: height * 0.175, // 17.5% of the screen height
+    bottom: -height * 0.15, 
+    left: width * 0.2,
+    borderRadius: height * 0.175, 
     transform: [{ scaleX: 2 }, { scaleY: 0.5 }],
   },
   tvscreenLeft: {
-    width: width * 0.5, // 10% of the screen width
-    height: height * 0.2, // 20% of the screen height
+    width: width * 0.5, 
+    height: height * 0.2, 
     backgroundColor: "#ff6347",
     position: "absolute",
-    left: -width * 0.05, // 5% of the screen width
-    top: height * 0.1, // 10% of the screen height
-    borderRadius: height * 0.1, // 10% of the screen height
+    left: -width * 0.05, 
+    top: height * 0.1, 
+    borderRadius: height * 0.1, 
     transform: [{ scaleX: 0.5 }, { scaleY: 2 }],
   },
   tvscreenRight: {
-    width: width * 0.5, // 10% of the screen width
-    height: height * 0.2, // 20% of the screen height
+    width: width * 0.5, 
+    height: height * 0.2, 
     backgroundColor: "#ff6347",
     position: "absolute",
-    right: -width * 0.05, // 5% of the screen width
-    top: height * 0.1, // 10% of the screen height
-    borderRadius: height * 0.1, // 10% of the screen height
+    right: -width * 0.05, 
+    top: height * 0.1, 
+    borderRadius: height * 0.1, 
     transform: [{ scaleX: 0.5 }, { scaleY: 2 }],
   },
 });
