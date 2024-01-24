16e51cf79ac2a11f07f1db5fd204b97dc38316d8
Yashwanth KM <yashwanth@perisync.com>
2024-01-24 09:37:57 +0530

    wed Morning

--- js/components/view/ToggleButtons.js
+++ js/components/view/ToggleButtons.js
@@ -1,18 +1,21 @@
 import React from 'react'
 import { flex, heightValue, marginPosition, radius, styles } from '../../styles/Styles'
-import {View,Text} from 'react-native'
+import {View,Text,TouchableOpacity} from 'react-native'
 
-export const ToggleButtons = () => {
+export const ToggleButtons = ({onPressProject,showProjects,onPressTags}) => {
   return (
    <View style={[styles.bgsmokeOrange,{height:heightValue(20)},marginPosition(10)]}>
     <View style={[styles.row,radius(10),flex(1),{backgroundColor:'#f5f5f5'}]}>
-        <View style={[flex(1),styles.allCenter,radius(5),styles.bgOrange]}>
-            <Text style={[styles.black,{fontWeight:'bold'}]}>Projects</Text>
+        <TouchableOpacity style={[flex(1)]} onPress={onPressProject}>
+        <View style={[flex(1),styles.allCenter,radius(5),showProjects?styles.bgOrange:null]}>
+            <Text style={[{fontWeight:'bold'},showProjects?styles.white:styles.black]}>Projects</Text>
         </View>
-        <View style={[flex(1),styles.allCenter,radius(5)]}>
-        <Text style={[styles.black,{fontWeight:'bold'}]}>Tags</Text>
-
+        </TouchableOpacity>
+        <TouchableOpacity style={[flex(1)]} onPress={onPressTags}>
+        <View style={[flex(1),styles.allCenter,radius(5),showProjects?null:styles.bgOrange]}>
+        <Text style={[{fontWeight:'bold'},showProjects?styles.black:styles.white]}>Tags</Text>
         </View>
+        </TouchableOpacity>
     </View>
    </View>
   )
