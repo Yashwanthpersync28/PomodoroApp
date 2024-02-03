04fefd223f7670054f7a012a7d44433be1980950
VinayKumarB8 <vinay@perisync.com>
2024-02-03 10:41:21 +0530

    dfgh

--- js/screens/Calendar/Components/DateSwiper.js
+++ js/screens/Calendar/Components/DateSwiper.js
@@ -6,79 +6,9 @@ import moment from 'moment'
 import Swiper from 'react-native-swiper'
 import Icon, { Icons } from '../../../components/Icons'
 
-//  export const DateSwiper = () => {
+export const DateSwiper = ({checkDate}) => {
 
-//   const swiper = useRef();
-//   const date = new Date()
-//   const Today = date.getDate();
-//   console.log('Todaydate Is :',Today)
-//   const [value,setValue] = useState(new Date());
-//   const [week,setWeek] = useState(0)
-//   console.log(value)
 
-//   const weeks = useMemo(()=>{
-//     const start = moment().add(week, 'weeks').startOf('isoWeek');
-//     return [-1,0,1].map(adj =>{
-//       return Array.from({length:7}).map((_,index)=>{
-//         const date = moment(start).add(adj,'week').add(index,'day');
-
-//         return{
-//           day:date.format('ddd'),
-//           date:date.toDate(),
-//         }
-//       })
-//     })
-//   },[week])
-
-//   return (
-//     <SafeAreaView style={[flex(1)]}>
-//       <View style={[styles.row,styles.centerHorizontal,flex(1),{width:widthValue(.8)}]}>
-//       <Swiper 
-//       index={1}
-//       loop={false} 
-//       showsPagination={false}
-//       ref={swiper}
-//       style={[{height:70,marginHorizontal:10}]}
-//       onIndexChanged={ind=>{
-//         if(ind === 1){
-//           return;
-//         }
-//         setTimeout(()=>{
-//           const newIndex = ind - 1;
-//           const newWeek = week + newIndex;
-//           setWeek(newWeek);
-//           setValue(moment(value).add(newIndex,'week').toDate())
-//           swiper.current.scrollTo(1,false);
-//         },100)
-//       }} >
-//        {weeks.map((dates,index)=>(
-//         <View style={[styles.row]} key={index}>
-//         {dates.map((item,dateIndex)=>{
-//           const isActive = value.toDateString() === item.date.toDateString();
-//           console.log(isActive,'isactive')
-//           return(
-//         <TouchableOpacity onPress={()=>{setValue(item.date)}} key={dateIndex}>
-//           <View style={[{width:50,height:60},isActive ? styles.bgtomotoRed : styles.bgWhite,radius(5),isActive? borderWidth(0): borderWidth(.5),styles.allCenter,margin(0,0,10)]}>
-          
-//         {Today ===item.date.getDate() && <Icon name={'star'} type={Icons.AntDesign}  style={[styles.Orange,fontSize(10),]} /> }
-            
-//             <Text style={[ isActive?styles.white:styles.black,fontSize(20),{fontWeight:'600'}]}>{item.date.getDate()}</Text>
-//             <Text style={[isActive?styles.white:styles.black,fontSize(16),{fontWeight:'400'}]}>{item.day}</Text>
-//             </View>
-//         </TouchableOpacity>
-        
-//           )
-//  })}</View>
-//        ))}
-//        </Swiper>
-//       </View>
-      
-//     </SafeAreaView>
-//   )
-// }
-// ... (your existing imports)
-
-export const DateSwiper = ({checkDate,value}) => {
   const swiper = useRef();
   const date = new Date();
   const Today = date.getDate();
@@ -99,6 +29,12 @@ export const DateSwiper = ({checkDate,value}) => {
       });
     });
   }, [week]);
+
+
+  const pressing = ()=>{
+    setValue(item.date);
+    console.log(value,'valuedkdnjb')
+  }
 
   return (
     <SafeAreaView style={[flex(1)]}>
@@ -129,12 +65,17 @@ export const DateSwiper = ({checkDate,value}) => {
                 console.log(isActive, 'isactive');
                 console.log(item.data)
                 return (
-                  <TouchableOpacity onPress={() => {
-                    setValue(item.date);
-                    checkDate();
-                    console.warn(value) // Assuming checkDate is a function you want to call
-                    console.log('Clicked Date:', item.date);
-                }} key={dateIndex}>
+                  <TouchableOpacity 
+                  
+                //   onPress={() => {
+                //     setValue(item.date);
+                //     checkDate();
+                //     console.warn(value) // Assuming checkDate is a function you want to call
+                //     console.log('Clicked Date:', item.date);
+                // }} 
+                onPress={pressing}
+                
+                key={dateIndex}>
                     <View style={[{ width: 44, height: 55 }, isActive ? styles.bgtomotoRed : styles.bgWhite, radius(5), isActive ? borderWidth(0) : borderWidth(0.5), styles.allCenter, margin(0, 0, 8)]}>
                       {Today === item.date.getDate() && (
                         <Icon name={'star'} type={Icons.AntDesign} style={[styles.Orange, fontSize(10)]} />
